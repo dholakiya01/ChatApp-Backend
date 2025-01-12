f4c91f0bb8e4e55ecb01fab252d49b12dfaba639
Dholakiya Darshan <darshandholakiya716@gmail.com>
2025-01-12 10:50:17 +0530

    clients access add in cors and vercel file

--- socket.js
+++ socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin: ['http://localhost:3000/','http://192.168.2.53:3000','http://192.168.1.106:3000'],
+        origin: ['http://localhost:3000/','http://192.168.2.53:3000','http://192.168.1.106:3000',"https://chat-app-frontend-nine-opal.vercel.app/"],
         methods:['GET','POST']
     }
 });
